e1bf15af750156972cdd8df3aaad5735746b4e06
Аркадий <Arrkaddiy@gmail.com>
2019-05-21 20:45:12 +0300

    V_1

--- src/ru/gunther/controllers/ExcelParserController.java
+++ src/ru/gunther/controllers/ExcelParserController.java
@@ -1,8 +1,11 @@
 package ru.gunther.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.stage.FileChooser;
@@ -18,16 +21,33 @@ public class ExcelParserController {
     private TableView<ExcelFile> excelParserTable;
     @FXML
     private TextArea excelParserTextArea;
+    @FXML
+    private ChoiceBox<String> buttonChoice;
 
     private FileChooser fileChooser = new FileChooser();
 
     public static ArrayList<ExcelFile> excelFiles = new ArrayList<>();
 
+
     public void addFile(ActionEvent actionEvent) {
-        List<File> files = fileChooser.showOpenMultipleDialog(((Node)actionEvent.getSource()).getScene().getWindow());
-        if (files != null) {
-            for (File file : files) excelFiles.add(new ExcelFile(files.indexOf(file)+1, file));
-        }
+        ArrayList<String> title = new ArrayList<>();
+        title.add("22");
+        title.add("11");
+
+
+
+        ObservableList<String> titleO = FXCollections.observableArrayList(title);
+
+        buttonChoice.getItems().addAll(titleO);
+
+
+//        List<File> files = fileChooser.showOpenMultipleDialog(((Node)actionEvent.getSource()).getScene().getWindow());
+//        if (files != null) {
+//            for (File file : files) excelFiles.add(new ExcelFile(files.indexOf(file)+1, file));
+//        }
+
+//        for (String we : title)
+//            buttonChoice.setValue(we);
 
         excelParserTextArea.appendText("Загруженно файлов : " + excelFiles.size() + "шт.\n");
     }
